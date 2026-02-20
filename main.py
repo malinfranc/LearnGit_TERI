e758f898df795b3aeb238d68243cba968c6c7043
Abraham Teri <e89186u@etu.univ-lorraine.fr>
2026-02-20 15:16:24 +0100

    Remise à normal

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ class Window(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        #self.setWindowTitle("Ma premiere fenetre")
+        self.setWindowTitle("Ma premiere fenetre")
         self.geometry()
         self.resize(400, 300)
 
