ce7be38efb995debb2979472cbdc309d59626cdf
Abraham Teri <e89186u@etu.univ-lorraine.fr>
2026-02-20 14:39:48 +0100

    creation de la fenetre

---
+++ main.py
@@ -0,0 +1,17 @@
+from PyQt5.QtWidgets import QMainWindow, QApplication
+import sys
+
+
+class Window(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("My First PyQt5 Window")
+        self.resize(400, 300)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    Fen = Window()
+    Fen.show()
+    sys.exit(app.exec_())  
